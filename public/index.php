e12e7817759c3e6f8ec419d8e58a42f716daf8b1
Tijs Verkoyen <tijs@verkoyen.eu>
2018-05-23 14:45:32 +0200

    Use dotenv also in production

--- public/index.php
+++ public/index.php
@@ -9,13 +9,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException(
-            'APP_ENV environment variable is not defined. You need to define environment 
-            variables for configuration or add "symfony/dotenv" as a Composer dependency to 
-            load variables from a .env file.'
-        );
-    }
     (new Dotenv())->load(__DIR__ . '/../.env');
 }
 
@@ -24,7 +17,6 @@ $debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
 
 if ($debug) {
     umask(0000);
-
     Debug::enable();
 }
 
